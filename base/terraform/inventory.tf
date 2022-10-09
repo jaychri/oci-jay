4a78b07ea821d22a9eaa7d247189026426cd1210
Jay Christopherson <jay.christopherson@emerson.com>
2022-10-09 16:07:51 -0500

    fixed ARM2 template file for ansible

--- base/terraform/inventory.tf
+++ base/terraform/inventory.tf
@@ -6,9 +6,9 @@ data "template_file" "inventory" {
     oci_core_instance.instance_arm2
   ]
   vars = {
-    inv_arm1      = "${var.instance_arm1_hostname} ansible_host=${oci_core_instance.instance_arm1.public_ip} ansible_user='ubuntu'"
-    inv_arm2      = "${var.instance_arm2_hostname} ansible_host=${oci_core_instance.instance_arm2.public_ip} ansible_user='ubuntu'"
-    inv_int1      = "${var.instance_int1_hostname} ansible_host=${oci_core_instance.instance_int1.public_ip} ansible_user='ubuntu'"
+    inv_arm1   = "${var.instance_arm1_hostname} ansible_host=${oci_core_instance.instance_arm1.public_ip} ansible_user='ubuntu'"
+    inv_arm2   = "${var.instance_arm2_hostname} ansible_host=${oci_core_instance.instance_arm2.public_ip} ansible_user='ubuntu'"
+    inv_int1   = "${var.instance_int1_hostname} ansible_host=${oci_core_instance.instance_int1.public_ip} ansible_user='ubuntu'"
     inv_global = " ansible_python_interpreter='/usr/bin/python3' \n ansible_ssh_private_key_file='/home/jay/code/oci-jay/base/key.pem'"
     inv_ubuntu = "${var.instance_arm1_hostname} \n ${var.instance_arm2_hostname} \n ${var.instance_int1_hostname}"
   }
